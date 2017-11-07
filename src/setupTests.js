fab9253a92ba7f5b2d95fe698a909f6ddac50b74
Matias Montenegro <matias@macbook.local>
2017-11-07 01:57:32 -0300

    Ejected

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,3 +1,22 @@
-import { configure } from 'enzyme';
-import Adapter from 'enzyme-adapter-react-15';
-configure({ adapter: new Adapter() });
+import Enzyme, { shallow, render, mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import toJson from 'enzyme-to-json';
+
+const raf = global.requestAnimationFrame = (cb) => {
+    setTimeout(cb, 0)
+}
+
+
+// React 16 Enzyme adapter
+Enzyme.configure({ adapter: new Adapter() });
+
+// Make Enzyme functions available in all test files without importing
+global.shallow = shallow;
+global.render = render;
+global.mount = mount;
+global.toJson = toJson;
+
+// Fail tests on any warning
+console.error = message => {
+    throw new Error(message);
+};
